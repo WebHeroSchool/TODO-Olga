a5a7ed3b3fe7a3f9d5a2e93d3e406e9de1d9e2a6
Ольга Мыльникова <olga.mylnikova.96@mail.ru>
2021-08-10 16:55:01 +0300

    Updates

---
+++ precache-manifest.9b4cdbc1d1d5102e056981ad1ecd6bf4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5589049cc9216c2aac888d06e904f42",
+    "url": "/TODO-Olga/index.html"
+  },
+  {
+    "revision": "25c0d477cc4380e34a3e",
+    "url": "/TODO-Olga/static/css/main.e7f23038.chunk.css"
+  },
+  {
+    "revision": "c8347ab9d2615778a17e",
+    "url": "/TODO-Olga/static/js/2.39f3cb4b.chunk.js"
+  },
+  {
+    "revision": "06ad0cfa56fa8a2c9ccc7b2fff0ec8e0",
+    "url": "/TODO-Olga/static/js/2.39f3cb4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25c0d477cc4380e34a3e",
+    "url": "/TODO-Olga/static/js/main.a99f6cd5.chunk.js"
+  },
+  {
+    "revision": "315fbaec4c80369f3acf",
+    "url": "/TODO-Olga/static/js/runtime-main.1be952f6.js"
+  }
+]);
